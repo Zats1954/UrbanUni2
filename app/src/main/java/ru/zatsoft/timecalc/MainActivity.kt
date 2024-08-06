a5a5580470611329af5d16d4c04d1b1ae3953c48
Zats1954 <alex.zacepin@gmail.com>
2024-08-06 22:03:24 +0200

    TimeCalc v.2

--- app/src/main/java/ru/zatsoft/timecalc/MainActivity.kt
+++ app/src/main/java/ru/zatsoft/timecalc/MainActivity.kt
@@ -1,16 +1,50 @@
 package ru.zatsoft.timecalc
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import ru.zatsoft.timecalc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var toolBar: Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        toolBar = binding.toolbarMain
+        setSupportActionBar( toolBar)
+        title = "Калькулятор времени"
+        toolBar.setSubtitle("            версия 1")
+        toolBar.setLogo(R.drawable.logo_48)
+        binding.tvResult.text = "Результат"
+        binding.tvResult.setTextColor(0x77000000)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.resetMenuMain -> {
+                binding.etFirstValue.text.clear()
+                binding.etSecondValue.text.clear()
+                binding.tvResult.text = "Результат"
+                binding.tvResult.setTextColor(0x77000000)
+                Toast.makeText(applicationContext, "Данные очищены", Toast.LENGTH_LONG).show()
+            }
+            R.id.exitMenuMain -> {finish()
+            Toast.makeText(applicationContext, "Приложение закрыто", Toast.LENGTH_LONG).show()
+        }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     fun onButtonClick(view: View){
@@ -19,12 +53,22 @@ class MainActivity : AppCompatActivity() {
         val second = toSeconds(binding.etSecondValue.text.toString())
 
         val res = when (view.id){
-            R.id.button -> Operation(first, second).sum()
-            R.id.button2 -> Operation(first, second).dif()
-            else -> 0
+            R.id.button -> {val result = Operation(first, second).sum()
+                makeResult(result)
+            }
+            R.id.button2 -> {val result = Operation(first, second).dif()
+                makeResult(result)
+            }
+            else -> " "
         }
-        val result = (res/60).toString() + "m" + (res % 60).toString() +"s"
-        binding.tvResult.text = result
+        binding.tvResult.text = res
+        binding.tvResult.setTextColor(0x778b0000)
+    }
+
+    private fun makeResult(result: Int): String {
+        val resultString = (result / 60).toString() + "m" + (result % 60).toString() + "s"
+        Toast.makeText(applicationContext, "Результат: $resultString", Toast.LENGTH_LONG).show()
+        return resultString
     }
 
     private fun toSeconds(minSec: String): Int {
